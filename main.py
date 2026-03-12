fd94e420930a337acd489a4f8509dc15c822b06b
Alfredo arteta <alfredoarteta03@gmail.com>
2026-03-12 17:40:45 -0500

    upload code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+print("bienvenido al programa de ventas")
+
+nommbre_producto = input("ingrese el nombre del producto: ")
+precio_producto = float(input("ingrese el precio del producto: "))    
+while precio_producto < 0:
+    print("el precio no puede ser negativo, por favor ingrese un precio valido")
+    precio_producto = float(input("ingrese el precio del producto: "))  
+cantidad_producto = int(input("ingrese la cantidad del producto: "))    
+while cantidad_producto < 0:
+    print("la cantidad no puede ser negativa, por favor ingrese una cantidad valida")
+    cantidad_producto = int(input("ingrese la cantidad del producto: "))
+
+total_venta = precio_producto * cantidad_producto
+print(f"usted ha comprado: producto: {nommbre_producto}, precio: {precio_producto}, cantidad: {cantidad_producto}")
+print(f"el total de la venta es: {total_venta}")
+
 
